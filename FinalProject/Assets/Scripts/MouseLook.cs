469d161da3d6c1745e9aa205e9b1fe4cf3184960
William Yang <wy2037@nyu.edu>
2022-11-07 22:47:26 -0500

    Updated Camera look + Inventory part 1

--- FinalProject/Assets/Scripts/MouseLook.cs
+++ FinalProject/Assets/Scripts/MouseLook.cs
@@ -11,14 +11,20 @@ public class MouseLook : MonoBehaviour
 
     public Transform player;
     float xRotation = 0f;
-
+    int touchIndex;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.touchCount > 0) {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Moved) {
+            Touch touchCheck = Input.GetTouch(0);
+            if (touchCheck.position.x > 800) {
+                touchIndex = 0;
+            } else {
+                touchIndex = Input.touchCount - 1;
+            }
+            Touch touch = Input.GetTouch(touchIndex);
+            if (touch.phase == TouchPhase.Moved && touch.position.x > 800) {
                 float touchX = touch.deltaPosition.x * sensitivity;
                 float touchY = touch.deltaPosition.y * sensitivity;
 
