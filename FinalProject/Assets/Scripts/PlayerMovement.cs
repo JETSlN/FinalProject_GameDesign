4abd22455b2cb1a8fd6047c917c9c27d558cb2ed
William Yang <wy2037@nyu.edu>
2022-11-12 01:18:12 -0500

    Start of Wall Running

--- FinalProject/Assets/Scripts/PlayerMovement.cs
+++ FinalProject/Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public FixedJoystick fixedJoystick;
     public CharacterController controller;
-    public Rigidbody rb;
+    //public Rigidbody rb;
 
     float speed;
     public float gravity = -9.81f;
@@ -37,10 +37,16 @@ public class PlayerMovement : MonoBehaviour
     public StaminaBar bar;
     bool runButtonPressed;
 
-
     // Update is called once per frame
     void Update()
     {
+        /*
+        CheckForWall();
+        WallRunInput();
+        camera.transform.localRotation = Quaternion.Euler(camera.localRotation.x, 10, wallCameraTilt);
+        orientation.transform.localRotation = Quaternion.Euler(0, 10, 0);
+        */
+
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
@@ -67,7 +73,6 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(velocity * Time.deltaTime);
     }
 
-
     void FixedUpdate()
     {
         bar.UpdateStaminaBar(50, stamina);
@@ -79,10 +84,84 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    /*
+    private void WallRunInput() {
+        if (fixedJoystick.Horizontal > 0.5f && wallRight && runButtonPressed && stamina > 0) {
+            StartWallRun();
+        } 
+        if (fixedJoystick.Horizontal < -0.5f && wallLeft && runButtonPressed && stamina > 0) {
+            StartWallRun();
+        } 
+    }
+
+    private void StartWallRun() {
+        gravity = 0f;
+        isWallRunning = true;
+
+        if (velocity.magnitude <= maxWallSpeed) {
+            velocity.z = orientation.forward * wallForce * Time.deltaTime;
+
+            if (wallRight) {
+                velocity.x = orientation.right * wallForce / 5 * Time.deltaTime;
+                if (Mathf.Abs(wallCameraTilt) < maxCameraTilt) {
+                    wallCameraTilt += Time.deltaTime * maxCameraTilt * 2;
+                }
+            } else {
+                velocity.x = orientation.left * wallForce / 5 * Time.deltaTime;
+                if (Mathf.Abs(wallCameraTilt) < maxCameraTilt) {
+                    wallCameraTilt -= Time.deltaTime * maxCameraTilt * 2;
+                }
+            }
+        }
+    }
+
+    private void StopWallRun() {
+        gravity = -9.81f;
+        isWallRunning = false;
+
+        if (wallCameraTilt > 0) {
+            wallRunCameraTilt -= Time.deltaTime * maxCameraTilt * 2f;
+        }
+        if (wallCameraTilt < 0) {
+            wallRunCameraTilt -= Time.deltaTime * maxCameraTilt * 2f;
+        }
+    }
+
+    private void CheckForWall() {
+        wallRight = Physics.Raycast(transform.position, orientation.right, 1f, wallCheck);
+        wallLeft = Physics.Raycast(transform.position, orientation.left, 1f, wallCheck);
+
+        if (!wallLeft && !wallRight) {
+            StopWallRun();
+        }
+    }
+    */
+
     public void Jump() {
         if (isGrounded) {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
+        /*
+        if (isWallRunning) {
+            if (wallLeft && fixedJoystick.Horizontal < 0.33f || wallRight && fixedJoystick.Horizontal > -0.33f) {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            if (wallRight || isWallLeft && fixedJoystick.Horizontal > 0.33f || fixedJoystick.Horizontal < -0.33f) {
+                velocity.y = Mathf.Sqrt(jumpHeight * 1f * gravity);
+            }
+
+            if (wallRight && fixedJoystick.Horizontal < -0.33f) {
+                velocity.x = Math.Sqrt(orientation.left * jumpHeight * 3.2f);
+            }
+
+            if (isWallLeft && fixedJoystick.Horizontal > 0.33f) {
+                velocity.x = Math.Sqrt(orientation.right * jumpHeight * 3.2f);
+            }
+
+            velocity.z = orientation.forward * jumpHeight;
+        }
+        */
     }
 
     public void RunHeld() {
