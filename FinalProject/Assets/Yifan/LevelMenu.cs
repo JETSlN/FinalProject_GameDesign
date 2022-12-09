b772b42d4f821515dcf9ba39a1764187138b0ead
metaknightccc <sunyifanc@126.com>
2022-12-09 12:12:14 -0500

    bgm for main and select scene

--- FinalProject/Assets/Yifan/LevelMenu.cs
+++ FinalProject/Assets/Yifan/LevelMenu.cs
@@ -14,18 +14,26 @@ public class LevelMenu : MonoBehaviour
     }
 
     public void PlayLevel2(){
+        SceneManager.MoveGameObjectToScene(GameObject.Find("Music"), SceneManager.GetActiveScene());
         SceneManager.LoadScene("Level__");
     }
     
     public void PlayLevel3(){
+        SceneManager.MoveGameObjectToScene(GameObject.Find("Music"), SceneManager.GetActiveScene());
         SceneManager.LoadScene("0");
     }
     
     public void PlayLevel4(){
+        SceneManager.MoveGameObjectToScene(GameObject.Find("Music"), SceneManager.GetActiveScene());
         SceneManager.LoadScene("Level X");
     }
 
     public void PlayLevel5(){
+        SceneManager.MoveGameObjectToScene(GameObject.Find("Music"), SceneManager.GetActiveScene());
         SceneManager.LoadScene("Office2");
+    }
+    public void Back(){
+        SceneManager.MoveGameObjectToScene(GameObject.Find("Music"), SceneManager.GetActiveScene());
+        SceneManager.LoadScene("Menu");
     }
 }
--- FinalProject/Assets/Yifan/MainMenu.cs
+++ FinalProject/Assets/Yifan/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
+        SceneManager.MoveGameObjectToScene(GameObject.Find("Music"), SceneManager.GetActiveScene());
         SceneManager.LoadScene("Tutorial");
     }
     
