ff691472ca3284256a9966f873a94cd83330775e
metaknightccc <sunyifanc@126.com>
2022-12-09 11:41:16 -0500

    stuff

--- FinalProject/Assets/Yifan/LevelMenu.cs
+++ FinalProject/Assets/Yifan/LevelMenu.cs
@@ -5,15 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class LevelMenu : MonoBehaviour
 {
+    public void PlayLevelTutotial(){
+        SceneManager.LoadScene("Tutorial");
+    }
+    
     public void PlayLevel1(){
         SceneManager.LoadScene("Level 1");
     }
-    
+
     public void PlayLevel2(){
+        SceneManager.LoadScene("Level__");
+    }
+    
+    public void PlayLevel3(){
+        SceneManager.LoadScene("0");
+    }
+    
+    public void PlayLevel4(){
         SceneManager.LoadScene("Level X");
     }
 
-    public void PlayLevel3(){
-        SceneManager.LoadScene("TwitterHQ");
+    public void PlayLevel5(){
+        SceneManager.LoadScene("Office2");
     }
 }
