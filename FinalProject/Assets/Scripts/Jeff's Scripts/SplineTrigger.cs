ab72967e9ffe6156edab6d0de9c6374927f014f3
JETSlN <68126549+JETSlN@users.noreply.github.com>
2022-12-09 10:37:09 -0500

    fixed stuff

--- FinalProject/Assets/Scripts/Jeff's Scripts/SplineTrigger.cs
+++ FinalProject/Assets/Scripts/Jeff's Scripts/SplineTrigger.cs
@@ -8,8 +8,13 @@ public class SplineTrigger : MonoBehaviour
 	public Transform myObject;
 	public float speed = 5f;
 	public float start = 0f;
+	public AudioSource audioSource;
+	public AudioClip cartyit;
+
 	public void call()
 	{   
+		GetComponent<AudioSource>().PlayOneShot(cartyit);
+
 		Tween.Spline (mySpline, myObject, start, 1, true, speed - (speed * start), 0, Tween.EaseInOut, Tween.LoopType.None);
 
 	}
--- FinalProject/Assets/Scripts/Jeff's Scripts/Touch_cart.cs
+++ FinalProject/Assets/Scripts/Jeff's Scripts/Touch_cart.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Touch_cart : MonoBehaviour
 {
+    public Transform cartyz; 
     public bool first_time = true;
     public GameObject player;
     public bool collided = false; 
@@ -18,7 +19,9 @@ public class Touch_cart : MonoBehaviour
     }
     private void Update() {
           if(collided == true){
+            
             player.GetComponent<StayCart>().trans();
+            
           }
 
     }
